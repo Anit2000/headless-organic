93eba775f32c39785e487560d2aaa047be201797
RonSparx <ron@sparxitsolutions.com>
2024-01-10 19:59:20 +0530

    sanity boiler updated

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'tf2ofztq',
+    projectId: 'ciyydlfz',
     dataset: 'production'
   }
 })
--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -5,9 +5,9 @@ import {schemaTypes} from './schemas'
 
 export default defineConfig({
   name: 'default',
-  title: 'Shopify - headless-organic-test',
+  title: 'studio-organic',
 
-  projectId: 'tf2ofztq',
+  projectId: 'ciyydlfz',
   dataset: 'production',
 
   plugins: [deskTool(), visionTool()],
